08dc9c242906d2417e4c1ea62d0620767de00695
cmmanik <mkmanik6523u@gmail.com>
2019-11-20 01:24:32 +0600

    add redux persist for store cart ites in localstoge

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,6 +1,16 @@
 import {combineReducers} from 'redux';
+
+import {persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
 import userReducer from './userReducer';
 import cartReducers from './cartReducers';
+
+const persistConfig = {
+    key:'root',
+    storage,
+    whitelist:['cart']
+}
 
 
 const rootReducer = combineReducers({
@@ -8,4 +18,4 @@ const rootReducer = combineReducers({
     cart:cartReducers
 })
 
-export default rootReducer;
+export default persistReducer(persistConfig, rootReducer)
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,10 @@
-import {createStore, applyMiddleware} from 'redux'
-import rootReducer from './reducers/rootReducer'
-import logger from 'redux-logger'
-const middleware = [logger]
-const store = createStore(rootReducer, applyMiddleware(...middleware))
-export default store;
+import {createStore, applyMiddleware} from 'redux';
+import rootReducer from './reducers/rootReducer';
+import {persistStore} from 'redux-persist'
+import logger from 'redux-logger';
+const middlewares = [logger];
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const persistor = persistStore(store);// // export default {store, peristStor};
+
+
+
